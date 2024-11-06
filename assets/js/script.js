c13c67e371b7a756b77ea57e113143a1881ac0ff
IsadoraTorres31 <isadoradamaiatorres@gmail.com>
2024-11-06 15:38:13 -0300

    mudanças no código

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// assets/js/script.js
+
+// Função para abrir o menu
+function openMenu() {
+    document.getElementById("sidebar").style.left = "0";
+}
+
+// Função para fechar o menu
+function closeMenu() {
+    document.getElementById("sidebar").style.left = "-250px";
+}
